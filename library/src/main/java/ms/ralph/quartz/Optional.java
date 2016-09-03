631bab939d1183e10ad09864e71d8030d3462c9f
Ralph <git@ralph.ms>
2016-09-03 18:43:37 +0900

    s/Require/Required/

--- library/src/main/java/ms/ralph/quartz/Optional.java
+++ library/src/main/java/ms/ralph/quartz/Optional.java
@@ -21,7 +21,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
+@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.CLASS)
 public @interface Optional {
 }
